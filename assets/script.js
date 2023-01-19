05739795a3e77262448fa80893921c1cd4bd9c1b
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-01-19 19:11:13 -0300

    Finalizado a munutenção de conexão

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+const usuario = [];
 // Painel lateral
 function aparecerPainelLateral(){
     const painel = document.querySelector("aside");
@@ -10,7 +11,8 @@ function esconderPainelLateral(){
 // Login
 function entrar(){
     const input = document.querySelector("#usuario");
-    const name = {name: (nome = input.value)};
+    let name = {name: (nome = input.value)};
+    usuario.push(name);
     if (nome !== ''){
         const login = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", name);
         login.then(entrarNoChat);
@@ -26,16 +28,15 @@ function entrarNoChat(){
 }
 function naoEntrou(erro){
     const statusCode = erro.response.status;
-    if (statusCode === 409){
+    if (statusCode === 400){
         alert("Nome em uso, digite outro nome");
     }
 }
-/* Manter conectado
+// Manter conectado
 function manterConectado(){
-    const input = document.querySelector("#usuario");
-    const name = {name: (nome = input.value)};
-    axios.post("https://mock-api.driven.com.br/api/v6/uol/status", name);
-} */
+    console.log(usuario[0]);
+    axios.post("https://mock-api.driven.com.br/api/v6/uol/status", usuario[0]);
+} 
 // Carregar as mensagens
 function buscarMensagens(){
     const promese = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
@@ -45,6 +46,16 @@ function buscarMensagens(){
 function carregarMensagens(resposta){
     console.log("FOII");
     console.log(resposta);
+    const mensagens = document.querySelector(".mensagens");
+    mensagens.innerHTML = "";
+    for (let contador = 0; contador < dada.length; contador++){
+        const li = `
+        <li class="${type}">
+            <p>${time}<strong>${from}</strong>${text}</p>
+        </li>
+        `;
+        mensagens.innerHTML += li;
+    }
 }
 // Carregar os participantes
 function buscarParticipantes(){
