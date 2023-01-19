714b25ac27bed9dd6354c1f32e9a0814ffe4f8e8
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-01-18 21:21:02 -0300

    funções de buscar mensagens e participantes

--- assets/script.js
+++ assets/script.js
@@ -15,24 +15,48 @@ function entrar(){
         const login = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", name);
         login.then(entrarNoChat);
         login.catch(naoEntrou);
-        setInterval(conectado, 5000);
     }
 }
 function entrarNoChat(){
     const logado = document.querySelector("section");
     logado.classList.add("esconder");
+    setInterval(manterConectado, 5000);
+    buscarMensagens();
+    buscarParticipantes();
 }
-function naoEntrou(){
-    alert("Nome em uso, digite outro nome");
+function naoEntrou(erro){
+    const statusCode = erro.response.status;
+    if (statusCode === 409){
+        alert("Nome em uso, digite outro nome");
+    }
 }
+/* Manter conectado
+function manterConectado(){
+    const input = document.querySelector("#usuario");
+    const name = {name: (nome = input.value)};
+    axios.post("https://mock-api.driven.com.br/api/v6/uol/status", name);
+} */
 // Carregar as mensagens
 function buscarMensagens(){
-    const promese = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
+    const promese = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     promese.then(carregarMensagens);
+    promese.catch(naoCarregou);
 }
 function carregarMensagens(resposta){
+    console.log("FOII");
     console.log(resposta);
 }
-// Manter conectado
-function conectado(){
+// Carregar os participantes
+function buscarParticipantes(){
+    const promese = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
+    promese.then(carregarParticipantes);
+    promese.catch(naoCarregou);
+}
+function carregarParticipantes(resposta){
+    console.log("FOII");
+    console.log(resposta);
+}
+function naoCarregou(erro){
+    console.log("DEU RUIM");
+    console.log(erro);
 }
